f64d1f8c0cc246bf2cf91b1a1199449881a441e9
Prateek Malhotra <someone1@gmail.com>
2016-12-15 17:56:27 -0500

    Update wazuh.sh

--- src/wazuh.sh
+++ src/wazuh.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-/run/entrypoint.sh $1
+set -m
+
+/run/miscellaneous/restore_config.sh
+/run/miscellaneous/edit_config.sh
+/run/miscellaneous/wait_for_elasticsearch.sh
+
 /usr/share/kibana/bin/kibana-plugin install http://wazuh.com/resources/wazuh-app.zip
+
+# Run as user "logstash" if the command is "kibana"
+if [ "$1" = 'kibana' ]; then
+	set -- gosu kibana tini -- "$@"
+fi
+$@ &
+
+fg

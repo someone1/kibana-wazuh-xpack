974598e6deedd2fafec2320a44f61c665245d48d
Prateek Malhotra <someone1@gmail.com>
2017-12-07 14:02:19 -0500

    Track entrypoint changes from upstream

--- src/wazuh.sh
+++ src/wazuh.sh
@@ -3,15 +3,10 @@
 set -m
 
 /run/miscellaneous/restore_config.sh
-/run/miscellaneous/edit_config.sh
 /run/miscellaneous/wait_for_elasticsearch.sh
 
 /usr/share/kibana/bin/kibana-plugin install http://wazuh.com/resources/wazuh-app.zip
 
-# Run as user "logstash" if the command is "kibana"
-if [ "$1" = 'kibana' ]; then
-	set -- gosu kibana tini -- "$@"
-fi
 $@ &
 
 fg
